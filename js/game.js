7b497a5eccd71a26639ae063ed83b6518dd5f049
svipwing <3260130869@qq.com>
2023-08-18 10:39:24 +0800

    [update] 彩蛋

--- js/game.js
+++ js/game.js
@@ -12,7 +12,7 @@ const targetImage = document.getElementById('target_image');
 // 彩蛋
 target.addEventListener('click', () => {
   const qqNumber = prompt('请输入QQ号:');
-  targetImage.src = 'https://q.qlogo.cn/g?b=qq&nk='+qqNumber+'&s=640';
+  targetImage.src = 'https://q.qlogo.cn/g?b=qq&nk=' + qqNumber + '&s=640';
 });
 
 let targetX = 50; // 靶子的X坐标
